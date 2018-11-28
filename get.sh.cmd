9811cf18be4b765f6a19722b89b464b123263147
ookk3388 <ookk3388@163.com>
2018-11-29 04:28:45 +0800

    create

---
+++ get.sh.cmd
@@ -0,0 +1,30 @@
+git submodule init
+
+git submodule add https://github.com/boob-sbcm/kotlin-dsl.git ./plugin-dev/kotlin-dsl
+
+git submodule add https://github.com/boob-sbcm/gradle-intellij-plugin.git ./plugin-dev/gradle-intellij-plugin #生成idea播件
+
+git submodule add https://github.com/boob-sbcm/gradle-node-envs.git ./plugin-dev/gradle-node-envs  #自动安装node
+
+git submodule add https://github.com/boob-sbcm/gradle-python-envs.git ./plugin-dev/gradle-python-envs #自动安装python
+
+
+git submodule add https://github.com/boob-sbcm/intellij-scala.git ./plugin-dev/idea-scala #集成scala
+
+git submodule add https://github.com/boob-sbcm/android.git ./plugin-dev/idea-android #集成android
+
+git submodule add https://github.com/boob-sbcm/gradle-idea-ext-plugin.git ./plugin-dev/gradle-idea-set #跟随idea设置
+
+git submodule add https://github.com/boob-sbcm/ExternalAnnotations.git ./tools/idea-annotations #外部注解件
+
+git submodule add https://github.com/boob-sbcm/plugin-repository-rest-client.git ./tools/plugin-rest命令行管理
+
+git submodule add https://github.com/boob-sbcm/IntelliJ-IDEA-Tutorial.git ./doc/idea-tutorial
+
+git submodule add https://github.com/boob-sbcm/robovm-studio.git ./sources/robovm-studio  #社区版vm
+
+git submodule add https://github.com/boob-sbcm/xodus.git ./database/xodus #idea通用数据库
+
+git submodule update
+
+git submodule sync
